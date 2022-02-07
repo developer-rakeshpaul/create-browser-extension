a33103f23fe260af188a91633aa53c1f1e61d31f
Rakesh Paul <rakesh@Rakeshs-MacBook-Pro.local>
2022-02-07 16:33:29 +0530

    Create browser extension initial commit

---
+++ react/src/background/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-red-900">
+         Background view
+      </h1>
+    </div>
+  )
+}
+
+export default App
